0ba4dae29b57eebc8a0c05e858c12ee3faf2fe9a
huliqun <huliquns@126.com>
2018-08-16 13:48:01 +0800

    fix

--- components/PageFooter.vue
+++ components/PageFooter.vue
@@ -4,7 +4,7 @@
 		<div>
 			<p>
 				&copy; Copyright  上海市申汇律师事务所 <br/>
-        <a href="http://shcainfo.miitbeian.gov.cn">沪ICP备18031519号</a><br/>
+        <a href="http://shcainfo.miitbeian.gov.cn">沪ICP备18031519号[沪ICP备18031519号-1]</a><br/>
 				技术支持: <a href="mailto:huliquns@126.com">Myway</a>
 			</p>
 		</div>
